b0b24c59150ee34591c3792cd9fafa6acd50a85a
10inoino <inomatahi@icloud.com>
2022-07-10 12:11:07 +0000

    headerの追加方法の変更

--- api/list-member.ts
+++ api/list-member.ts
@@ -1,7 +1,22 @@
 import fetch from 'node-fetch';
 
-export default async function handler(request, response) {
-  const res = await fetch(`https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`, {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = async (req, res) => {
+  const result = await fetch(`https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`, {
     method: 'POST',
     headers: { 
         'Content-Type': 'application/json',
@@ -10,10 +25,7 @@ export default async function handler(request, response) {
     },
   });
 
-  const data = await res.json();
-
-  response.setHeader('Access-Control-Allow-Credentials', true)
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  return response.status(200).json({ data });
+  return await result.json();
 }
+
+module.exports = allowCors(handler)
