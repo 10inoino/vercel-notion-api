6b4002330b0d5afeae7dafa221f6ca0884eb8400
10inoino <inomatahi@icloud.com>
2022-07-10 11:42:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 参考資料
+- https://kotsukotsu.work/tech/2020-08-27-vercel-serverless-functions-web-api-%E3%82%92%E3%83%87%E3%83%97%E3%83%AD%E3%82%A4%E3%81%99%E3%82%8B/
---
+++ api/list-member.ts
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+export default async function handler(request, response) {
+  const res = await fetch(`https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`, {
+    method: 'POST',
+    headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.SECRET_TOKEN}`,
+        'Notion-Version': '2021-08-16',
+    },
+  });
+
+  const data = await res.json();
+  return response.status(200).json({ data });
+}
